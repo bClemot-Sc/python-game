57a8f203288d2efca6d82ccea6c3c7deb49b649e
Bastien Clémot <bclemot85@gmail.com>
2023-12-27 17:43:00 +0100

    Generate window
    Initialize pygame

---
+++ little-game.py
@@ -0,0 +1,15 @@
+### Little game with insects ###
+## Author : Bastien Clémot
+
+
+## ----- Libraries
+import pygame
+import sys
+
+
+## ----- Window initialization
+
+# Start game
+pygame.init()
+
+# 
